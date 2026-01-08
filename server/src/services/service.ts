2c62d9fc0deed4f96c974a3cb88d30575005f3b0
vrtxLeonie <leonie@vrtxlabs.com>
2026-01-08 17:09:43 +0100

    Convert lifecycle hooks to document service middleware

--- server/src/services/service.ts
+++ server/src/services/service.ts
@@ -8,36 +8,62 @@ const service = ({ strapi }: { strapi: Core.Strapi }) => ({
   getWelcomeMessage() {
     return 'Welcome to Strapi 🚀';
   },
-  handleCRUDOperation(event: any) {
+  async handleCRUDOperation(context: any) {
     const errorMessages: any = {
       inner: [],
     };
 
-    Object.keys(event.model.attributes).forEach((attribute) => {
-      if (event.model.attributes[attribute].customField === 'plugin::strapi-advanced-uuid.uuid') {
-        // Get the initial value of the attribute
-        const initialValue = event.params.data[attribute];
+    for (const attribute of Object.keys(context.contentType.attributes)) {
+      if (context.contentType.attributes[attribute].customField !== 'plugin::strapi-advanced-uuid.uuid') continue;
 
-        // Get the options of the attribute
-        const options = event.model.attributes
-          ? event.model.attributes[attribute]['options']
-          : null;
+      // Get the initial value of the attribute
+      const givenValue = context.params.data ? context.params.data[attribute] : null;
+      let currentValue = givenValue;
 
-        // Get the uuid-format option, if it is set
-        const uuidFormat = options ? options['uuid-format'] : null;
-        // Get the disable-auto-fill option, if it is set
-        const disableAutoFill = options ? options['disable-auto-fill'] : false;
+      // if we update and the attribute is not given we want to get it from the database
+      if (context.action == 'update' && !givenValue) {
+        const documentId = context.params.data?.documentId ? context.params.data.documentId : context.params.documentId;
+        const hasDraftAndPublish = context.contentType.options?.draftAndPublish as boolean;
 
-        // If there is no initial value and disableAutoFill is not enabled, generate a new UUID
-        if (!event.params.data[attribute] && !disableAutoFill) {
-          event.params.data[attribute] = generateUUID(uuidFormat);
+        //drafts should be most up to date
+        const drafts = await strapi.db.query(context.uid).findMany({
+          where: {
+            documentId,
+            publishedAt: { $null: hasDraftAndPublish },
+          },
+        });
+        if (drafts.length <= 0) {
+          strapi.log.error("Could not find original draft to determine original value of " + attribute);
+          return;
         }
+
+        currentValue = drafts[0][attribute];
+      }
+
+      // Get the options of the attribute
+      const options = context.contentType.attributes
+        ? context.contentType.attributes[attribute]['options']
+        : null;
+
+      // Get the uuid-format option, if it is set
+      const uuidFormat = options ? options['uuid-format'] : null;
+      // Get the disable-auto-fill option, if it is set
+      const disableAutoFill = options ? options['disable-auto-fill'] : false;
+
+      // If there is no initial value and disableAutoFill is not enabled, generate a new UUID
+      if (!currentValue && !disableAutoFill) {
+        const newUUID = generateUUID(uuidFormat);
+        if (!context.params.data)
+          context.params['data'] = {};
+        context.params.data[attribute] = newUUID;
+
+        currentValue = newUUID;
 
         // Validation happens on following conditions:
         // - If disableAutoFill is not enabled
         // - If there is an initial value
-        if (!disableAutoFill || initialValue) {
-          if (!isValidUUIDValue(uuidFormat, event.params.data[attribute])) {
+        if (!disableAutoFill || currentValue) {
+          if (!isValidUUIDValue(uuidFormat, currentValue)) {
             errorMessages.inner.push({
               name: 'ValidationError', // Always set to ValidationError
               path: attribute, // Name of field we want to show input validation on
@@ -46,7 +72,7 @@ const service = ({ strapi }: { strapi: Core.Strapi }) => ({
           }
         }
       }
-    });
+    }
 
     if (errorMessages.inner.length > 0) {
       throw new YupValidationError(errorMessages, 'You have some issues');
