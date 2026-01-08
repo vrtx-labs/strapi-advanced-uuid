37aff91895ab0bed9e90de5a4490621755ebcc5f
vrtxLeonie <leonie@vrtxlabs.com>
2026-01-08 17:28:41 +0100

    Remove unnecessary value lookup
    If an update is happening, we do not need to check the saved uuid again, as it was already validated when creating the entity.

--- server/src/services/service.ts
+++ server/src/services/service.ts
@@ -17,28 +17,7 @@ const service = ({ strapi }: { strapi: Core.Strapi }) => ({
       if (context.contentType.attributes[attribute].customField !== 'plugin::strapi-advanced-uuid.uuid') continue;
 
       // Get the initial value of the attribute
-      const givenValue = context.params.data ? context.params.data[attribute] : null;
-      let currentValue = givenValue;
-
-      // if we update and the attribute is not given we want to get it from the database
-      if (context.action == 'update' && !givenValue) {
-        const documentId = context.params.data?.documentId ? context.params.data.documentId : context.params.documentId;
-        const hasDraftAndPublish = context.contentType.options?.draftAndPublish as boolean;
-
-        //drafts should be most up to date
-        const drafts = await strapi.db.query(context.uid).findMany({
-          where: {
-            documentId,
-            publishedAt: { $null: hasDraftAndPublish },
-          },
-        });
-        if (drafts.length <= 0) {
-          strapi.log.error("Could not find original draft to determine original value of " + attribute);
-          return;
-        }
-
-        currentValue = drafts[0][attribute];
-      }
+      let initialValue = context.params.data ? context.params.data[attribute] : null;
 
       // Get the options of the attribute
       const options = context.contentType.attributes
@@ -51,25 +30,26 @@ const service = ({ strapi }: { strapi: Core.Strapi }) => ({
       const disableAutoFill = options ? options['disable-auto-fill'] : false;
 
       // If there is no initial value and disableAutoFill is not enabled, generate a new UUID
-      if (!currentValue && !disableAutoFill) {
+      if (!initialValue && !disableAutoFill) {
         const newUUID = generateUUID(uuidFormat);
         if (!context.params.data)
           context.params['data'] = {};
         context.params.data[attribute] = newUUID;
 
-        currentValue = newUUID;
+        initialValue = newUUID;
+      }
 
-        // Validation happens on following conditions:
-        // - If disableAutoFill is not enabled
-        // - If there is an initial value
-        if (!disableAutoFill || currentValue) {
-          if (!isValidUUIDValue(uuidFormat, currentValue)) {
-            errorMessages.inner.push({
-              name: 'ValidationError', // Always set to ValidationError
-              path: attribute, // Name of field we want to show input validation on
-              message: 'The UUID format is invalid.', // Input validation message
-            });
-          }
+      // Validation happens on following conditions:
+      // - If disableAutoFill is not enabled
+      // - If there is an initial value
+      if (!disableAutoFill || initialValue) {
+        if (!isValidUUIDValue(uuidFormat, initialValue)) {
+          errorMessages.inner.push({
+            name: 'ValidationError', // Always set to ValidationError
+            path: attribute, // Name of field we want to show input validation on
+            message: 'The UUID format is invalid.', // Input validation message
+          });
+
         }
       }
     }
